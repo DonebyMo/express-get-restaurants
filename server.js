d6f5b919b155f41b55b081967083eb3fea5ccbf4
Muhammad <muhammadalnabhani@gmail.com>
2023-02-23 11:02:52 +0000

    created route handlers that create, update and delete resources from restaurants’ database.

--- server.js
+++ server.js
@@ -28,6 +28,26 @@ app.get("/restaurants/:id", async (req, res) => {
   res.send(restaurants);
 });
 
+app.use(express.json());
+
+app.post("/restaurants/:id", async (req, res) => {
+  const restaurant1 = await Restaurant.create(req.body);
+  await res.send(restaurant1);
+});
+
+app.put("/restaurants/:id", async (req, res) => {
+  const restaurant = await Restaurant.update(
+    { name: "The best" },
+    { where: { id: req.params.id } }
+  );
+  await res.send(restaurant);
+});
+
+app.delete("/restaurants/:id", async (req, res) => {
+  await Restaurant.destroy({ where: { id: req.params.id }, force: true });
+  await res.send("Successful DELETE Request made!");
+});
+
 app.listen(port, () => {
   sequelize.sync();
   console.log("Your server is listening on port " + port);
